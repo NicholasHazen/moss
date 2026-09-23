776dcec5fab903575ad73aacf424b417b24d9111
Nick Hazen <nicholasahazen@gmail.com>
2026-09-23 08:09:41 -0700

    Refine textbook explanations and navigation after independent reviews

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-0dabd431.js',
+                path_to_root + 'searchindex-e8e1b246.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
